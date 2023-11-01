591fab847ecf866bc60accb1bb133c0babad9951
Jared Parrish <jared.crane.parrish@gmail.com>
2023-11-01 23:58:21 +0000

    fix redirect isssue

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 function login() {
-  const nameEl = document.querySelector("#name");
-  localStorage.setItem("userName", nameEl.value);
-  window.location.href = "day.html";
+  const nameEl = document.querySelector('#name');
+  localStorage.setItem('userName', nameEl.value);
+  window.location.href = 'day.html';
 }
