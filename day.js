0dc6f10fd2f7f5a0502f919ff3b512cb6f79ec50
Jared Parrish <jared.crane.parrish@gmail.com>
2023-11-02 02:38:35 +0000

    debugged update and refresh and made new cells editable

--- day.js
+++ day.js
@@ -1,5 +1,5 @@
 function selectDay() {
-    const dayEl = document.querySelector("#Day");
+    const dayEl = document.querySelector("#selectedDay");
     localStorage.setItem("selectedDay", dayEl.value);
 }
 
@@ -7,53 +7,59 @@ function refresh() {
     selectDay();
     const selectedDay = localStorage.getItem("selectedDay");
     let calendar = [];
-    const calendarText = localStorage.getItem(selectedDay + ".calendar");
+    const calendarText = localStorage.getItem("Thursday.calendar");
     if (calendarText) {
         calendar = JSON.parse(calendarText);
     }
 
-    if (calendar.length) {
-        for (const [i, row] of calendar.entries()) {
+
+    if (Object.keys(calendar).length !== 0 ) {
+        for (const [id, row] of Object.entries(calendar)) {
+            if (id === "head"){
+                continue;
+            }
             const hourTdEl = document.createElement('td');
             const hostTdEl = document.createElement('td');
-            const activitytdEL = document.createElement('td');
+            hostTdEl.setAttribute("contenteditable", "true");
+            const activitytdEL = document.createElement('td', {is: contenteditable='true'});
+            activitytdEL.setAttribute("contenteditable", "true");
 
-            const id = row.id;
-
-            if (row.hasOwnProperty(host)) {
-                hourTdEl.textContent = row.hour;
-                hostTdEl.textContent = row.host;
-                activitytdEL.textContent = row.activity;
-            } else {
-                hourTdEl.textContent = row.hour;
-                hostTdEl.textContent = '';
-                activitytdEL.textContent = '';
-            }
+            hourTdEl.textContent = row.hour;
+            hostTdEl.textContent = row.host;
+            activitytdEL.textContent = row.activity;
             
             const rowEl = document.createElement('tr');
             rowEl.appendChild(hourTdEl);
             rowEl.appendChild(hostTdEl);
             rowEl.appendChild(activitytdEL);
 
-            document.getElementById(id).innerHTML = rowEl.innerHTML;
+            document.getElementById(id).replaceWith(rowEl);
         }
     }
 }
 
 function update() {
     selectDay();
-    const rowIds = ["8AM","9AM","10AM","11AM","12AM","1PM","2PM",
-         "3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
+    const rowIds = ["head","8AM","9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM","6PM","7PM","8PM","9PM","10PM","11PM"];
 
     const selectedDay = localStorage.getItem("selectedDay");
     let calendarObj = {};
+    calendarObj.head = {hour:"Time", host:"Host", activity: "activity"};
     const table = document.getElementById('calendar');
 
-    for (const [i, id] of rowIds){
-        calendarObj[id] = { hour: table.rows[i+1].cells[0].innerHTML,
-                            host: table.rows[i+1].cells[1].innerHTML,
-                            activity: table.rows[i+1].cells[2].innerHTML
-        };
+    for (i in rowIds){
+        if (table.rows[i].cells.length === 1){
+                calendarObj[rowIds[i]] = { hour: table.rows[i].cells[0].innerHTML };
+        } else{
+            if (table.rows[i].cells.length === 2){
+                calendarObj[rowIds[i]] = { hour: table.rows[i].cells[0].innerHTML, 
+                                activity: table.rows[i].cells[2].innerHTML };
+            } else {
+                calendarObj[rowIds[i]] = { hour: table.rows[i].cells[0].innerHTML, 
+                                    host: table.rows[i].cells[1].innerHTML,
+                                activity: table.rows[i].cells[2].innerHTML };
+            }
+        }
     }
     localStorage.setItem(selectedDay + ".calendar", JSON.stringify(calendarObj));
 }
