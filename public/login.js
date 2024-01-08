1cdaa4b88ab7169da47fd327b7c689219c9ce0ec
Jared Parrish <jared.crane.parrish@gmail.com>
2024-01-08 21:28:34 +0000

    added basic code for mongodb

--- public/login.js
+++ public/login.js
@@ -3,3 +3,24 @@ function login() {
   localStorage.setItem('userName', nameEl.value);
   window.location.href = 'day.html';
 }
+
+function displayQuote(data) {
+  fetch('https://api.quotable.io/random')
+    .then((response) => response.json())
+    .then((data) => {
+      const containerEl = document.querySelector('#quote');
+
+      const quoteEl = document.createElement('p');
+      quoteEl.classList.add('quote');
+      const authorEl = document.createElement('p');
+      authorEl.classList.add('author');
+
+      quoteEl.textContent = data.content;
+      authorEl.textContent = data.author;
+
+      containerEl.appendChild(quoteEl);
+      containerEl.appendChild(authorEl);
+    });
+}
+
+displayQuote();
